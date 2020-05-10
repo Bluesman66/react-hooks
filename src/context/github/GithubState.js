aa04beb02c4d1ed1385204103f77fb1ef273e276
alex.paclin <alex.paclin@gmail.com>
2020-05-10 10:37:43 +0500

    11. Настройка запросов

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -14,6 +14,10 @@ import githubReducer from './githubReducer';
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
+const withCreds = (url) => {
+	return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+};
+
 const GithubState = ({ children }) => {
 	const initialState = {
 		user: {},
@@ -26,26 +30,32 @@ const GithubState = ({ children }) => {
 	const search = async (value) => {
 		setLoading();
 		const res = await axios.get(
-			`https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+			withCreds(`https://api.github.com/search/users?q=${value}&`)
 		);
 		dispatch({ type: SEARCH_USERS, payload: res.data.items });
 	};
 
 	const getUser = async (name) => {
 		setLoading();
-		dispatch({ type: GET_USER, payload: {} });
+		const res = await axios.get(
+			withCreds(`https://api.github.com/users/${name}?`)
+		);
+		dispatch({ type: GET_USER, payload: res.data });
 	};
 
 	const getRepor = async (name) => {
 		setLoading();
-		dispatch({ type: GET_REPOS, payload: [] });
+		const res = await axios.get(
+			withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+		);
+		dispatch({ type: GET_REPOS, payload: res.data });
 	};
 
 	const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
 	const setLoading = () => dispatch({ type: SET_LOADING });
 
-	const { user, sers, loading, repos } = state;
+	const { user, users, loading, repos } = state;
 
 	return (
 		<GithubContext.Provider
@@ -56,7 +66,7 @@ const GithubState = ({ children }) => {
 				clearUsers,
 				setLoading,
 				user,
-				sers,
+				users,
 				loading,
 				repos,
 			}}
